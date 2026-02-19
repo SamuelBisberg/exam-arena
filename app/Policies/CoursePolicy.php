8ff9ec7934948014aed4b042f87c29709bafd4f1
Shmuel Bisberg <shmuelbisberg.acc@gmail.com>
2026-02-19 18:18:12 +0200

    Add CoursePolicy and UnivercityPolicy for access control

--- app/Policies/CoursePolicy.php
+++ app/Policies/CoursePolicy.php
@@ -2,7 +2,23 @@
 
 namespace App\Policies;
 
+use App\Enums\CourseActivityStatusEnum;
+use App\Enums\PermissionsEnum;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+use Illuminate\Database\Eloquent\Model;
+
 class CoursePolicy extends BasePolicy
 {
-    //
+    public function viewAny(User $user): Response
+    {
+        return Response::allow();
+    }
+
+    public function view(User $user, Model $course): Response
+    {
+        return $course->status === CourseActivityStatusEnum::ACTIVE || $user->can(PermissionsEnum::VIEW_HIDDEN_CONTENT) ?
+            Response::allow() :
+            Response::denyAsNotFound();
+    }
 }
---
+++ app/Policies/UnivercityPolicy.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+use Illuminate\Database\Eloquent\Model;
+
+class UnivercityPolicy extends BasePolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): Response
+    {
+        return Response::allow();
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Model $model): Response
+    {
+        return Response::allow();
+    }
+}
